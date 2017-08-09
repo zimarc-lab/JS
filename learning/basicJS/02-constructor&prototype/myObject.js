1312c27f7247a339a3306c6b2864ea9f89b7bda5
maysam <Maysssam@gmail.com>
2017-08-09 07:25:22 -0700

    vccv

--- learning/basicJS/02-constructor&prototype/myObject.js
+++ learning/basicJS/02-constructor&prototype/myObject.js
@@ -15,7 +15,7 @@ function global(){
     return this; // This will setp out one and point to window context
 }
 
-
+//CONSTRUCTOR
 //Constructor : the are like a blue print to create an instance  of  an object. 
 // they are like template. create object with diffrente value 
 // naming convention for constructore function started  with capital word to be idetifyable
@@ -23,11 +23,38 @@ function global(){
 // new keyword: for creating an instance of the object 
 
 function Apple(x,y,color,score){
-
+// unique properties for each apple 
     this.x = x;
     this.y = y;
     this.color = color;
     this.score = score;
+// eat an throw are NOT unique and they dont need to be in every instance apple
+
+   // this.eat = function(){return "eat the apple";}; ----> moved to prototype
+   // this.throw = function(){return "throw the apple";}; -----> moved to prototype
 }
 
-new Apple(10,20,"red",300);
+
+//PROTOTYPE
+//prototypes are shared object, prop and methode that share 
+//The constructor manufactures new objects
+//The prototype defines the behaviour of new objects manufactured by the constructor
+//THis is about context 
+
+
+Apple.prototype = {
+   //--->
+    eat : function(){ return "eat the apple";}, 
+    throw : function(){ return "throw the apple";}
+}
+
+
+var apple01 = new Apple(10,20,"red",300);
+var apple02 = new Apple(10,20,"red",300);
+
+
+//THIS 
+///eat : function(){ return this;}, 
+//apple2.eat
+// this will point to apple2 object , beacuse called up by apple02. it will back up to apple02
+//
